c03e8322a646cce978c6ed304de2bbd92e6378c4
tush87 <tusharj8788@gmail.com>
2023-05-26 15:13:25 +0530

    add new contact functionality

--- src/App.js
+++ src/App.js
@@ -14,12 +14,44 @@ class App extends React.Component {
     }
   }
 
+  //add new contact
+  addNewContact = (e)=>{
+
+     e.preventDefault();
+    
+     let {contact} = this.state;
+   
+     // Read the form data
+     const form = e.target;
+     const formData = new FormData(form);
+   
+     // You can pass formData as a fetch body directly:
+     fetch('https://jsonplaceholder.typicode.com/users', { method: form.method, body: formData });
+   
+    //  // You can generate a URL out of it, as the browser does by default:
+    //  console.log(new URLSearchParams(formData).toString());
+    //  // You can work with it as a plain object.
+     const formJson = Object.fromEntries(formData.entries());
+
+     formJson.id=Date.now();
+    
+     console.log(formJson); // (!) This doesn't include multiple select values
+     // Or you can get an array of name-value pairs.
+    //  console.log([...formData.entries()]);
+    
+    contact.unshift(formJson);
+    
+    this.setState({
+      contact
+    })
+  }
+
   //render
   render() {
     return (
       <div className="App">
         hello
-        <NewContact/>
+        <NewContact onNewContact={this.addNewContact}/>
         <ContactList contacts={this.state.contact} />
       </div>
     )
--- src/NewContact.js
+++ src/NewContact.js
@@ -1,9 +1,13 @@
+
 let NewContact = (props) => {
+
+    let {onNewContact} = props;
+
     return(
         <div>
-            <form>
-                <input type="text" placeholder="Enter Name"/>
-                <input type="number" placeholder="Enter Phone"/>
+            <form method="post" onSubmit={(e)=>{onNewContact(e)}}>
+                <input type="text" name="name" placeholder="Enter Name" required/>
+                <input type="number" name="phone" placeholder="Enter Phone" required/>
                 <input type="submit" value="Add"/>
             </form>
         </div>
