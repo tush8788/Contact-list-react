4611f676bc31b01d7adcc557a5f9036e9456e8e3
tush87 <tusharj8788@gmail.com>
2023-05-26 12:52:50 +0530

    convert app function to class function and add state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       hello
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      contact:[{
+        name:"Test",
+        phone:1234567890
+      }]
+    }
+  }
+
+  //render
+  render(){
+    return (
+      <div className="App">
+         hello
+      </div>
+    )
+  }
+
 }
-
 export default App;
